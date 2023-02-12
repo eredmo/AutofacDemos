8b46a70a3bbe9e60bbc7fb6531db8e09dc706bdf
Esbjörn Redmo <esbjorn.redmo@gmail.com>
2023-02-12 21:00:14 +0100

    Added some code

---
+++ 01 Basic Setup/Program.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+
+namespace AD01;
+
+internal static class Program
+{
+    internal static void Main()
+    {
+        // Build container
+        var cb = new ContainerBuilder();
+        cb.RegisterType<Greeter>();
+        using var container = cb.Build();
+
+        // Use container
+        var greeter = container.Resolve<Greeter>();
+        greeter.Greet();
+    }
+}
+
+public sealed class Greeter
+{
+    public void Greet()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ 02 Simple Lifetime/Program.cs
@@ -0,0 +1,49 @@
+﻿using Autofac;
+
+namespace AD02;
+
+internal static class Program
+{
+    internal static void Main()
+    {
+        // Build container
+        var cb = new ContainerBuilder();
+        cb.RegisterType<Greeter>();
+        using IContainer container = cb.Build();
+
+        // Create a lifetime
+        using (var lifetime = container.BeginLifetimeScope())
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var greeter = lifetime.Resolve<Greeter>();
+                greeter.Greet();
+            }
+        }
+
+        Console.WriteLine("Done");
+        Console.ReadLine();
+    }
+}
+
+
+public sealed class Greeter //: IDisposable
+{
+    private readonly byte[] _tmp = new byte[100000000];
+
+    public void Greet()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    //~Greeter()
+    //{
+    //    Console.WriteLine("Finalizer called...");
+    //}
+
+    //public void Dispose()
+    //{
+    //    GC.SuppressFinalize(this);
+    //    Console.WriteLine("Dispose called...");
+    //}
+}
